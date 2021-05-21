7ec2a10e9e1e4b9cd4215b133393ee14ec36c159
JulianaOnofrio <julianaonofrio@hotmail.com>
2021-05-21 01:17:39 -0300

    Criando o setup do projeto

---
+++ casa-do-codigo/src/test/java/br/com/juliana/casadocodigo/CasaDoCodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.juliana.casadocodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaDoCodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
