a438cd0bd52f3a6dcacd3279c9fa5ebcdd81ac61
JulianaOnofrio <julianaonofrio@hotmail.com>
2021-05-27 02:03:44 -0300

    Implementing new author registration

--- casa-do-codigo/src/main/java/br/com/juliana/casadocodigo/exception/handler/NotFoundExceptionHandler.java
+++ casa-do-codigo/src/main/java/br/com/juliana/casadocodigo/exception/handler/NotFoundExceptionHandler.java
@@ -1,6 +1,5 @@
 package br.com.juliana.casadocodigo.exception.handler;
 
-import br.com.juliana.casadocodigo.exception.NotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
